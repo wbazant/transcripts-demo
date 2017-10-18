ce907f2384d0fe80220f9d0a32ebc70209c63e5e
wbazant <wbazant@ebi.ac.uk>
2017-10-18 17:04:51 +0100

    Done: - add zoom - start the y axis from 0 to the highest number and a little bit - make the y axis logarithmic - remove the credits

--- src/index.js
+++ src/index.js
@@ -13,11 +13,16 @@ import 'react-select/dist/react-select.css';
 
 const highcharts = ({xAxisCategories, dataSeries}) => ({
     chart: {
-        type: 'boxplot'
+        type: 'boxplot',
+		zoomType: 'x'
     },
 
     title: {
         text: ''
+    },
+
+	credits: {
+        enabled: false
     },
 
     legend: {
@@ -31,10 +36,13 @@ const highcharts = ({xAxisCategories, dataSeries}) => ({
         }
     },
 
-    yAxis: [{
+    yAxis: {
         title: {
-            text: 'Expression value'
-        }}]
+            text: 'Expression (TPM)'
+        },
+		type:'logarithmic',
+		min:0.1
+	}
     ,
 
     plotOptions: {
@@ -94,178 +102,3 @@ const Main = ({columnHeaders, profiles:{rows}}) => (
 )
 
 export default Main
-
-const fetchResponseJson = async (base, endpoint) => {
-  const response = await fetch(URI(endpoint, base).toString())
-  const responseJson = await response.json()
-  return responseJson
-}
-
-class _Main extends React.Component {
-	constructor(props) {
-      super(props)
-      this.state = {
-        loaded: false,
-		data: {}
-      }
-    }
-
-	render() {
-	  const {height, atlasUrl, resourcesUrl} = this.props
-	  const {suggesterEndpoint, geneId, highlightClusters, ks, k, perplexities, perplexity} = this.props
-	  const {onChangePerplexity, onChangeK, onSelectGeneId} = this.props
-	  const {loadingClusters, loadingGeneExpression, data, errorMessage} = this.state
-
-	  return (
-		<div className={`row`}>
-		  <div className={`small-12 medium-6 columns`}>
-			<ClusterTSnePlot height={height}
-							 plotData={data}
-							 perplexities={perplexities}
-							 perplexity={perplexity}
-							 onChangePerplexity={onChangePerplexity}
-							 ks={ks}
-							 k={k}
-							 onChangeK={onChangeK}
-							 highlightClusters={highlightClusters}
-							 loading={loadingClusters}
-							 resourcesUrl={resourcesUrl}
-							 errorMessage={errorMessage}
-			/>
-		  </div>
-
-		  <div className={`small-12 medium-6 columns`}>
-			<GeneExpressionTSnePlot height={height}
-									plotData={data}
-									atlasUrl={atlasUrl}
-									suggesterEndpoint={suggesterEndpoint}
-									onSelectGeneId={onSelectGeneId}
-									geneId={geneId}
-									highlightClusters={highlightClusters}
-									loading={loadingGeneExpression}
-									resourcesUrl={resourcesUrl}
-									errorMessage={errorMessage}
-			/>
-		  </div>
-
-		</div>
-	  )
-	}
-
-
-}
-
-class ExperimentPageView extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      data: {
-        series: [],
-        unit: ``
-      },
-      errorMessage: null,
-      loadingClusters: false,
-      loadingGeneExpression: false
-    }
-  }
-
-  _fetchAndSetState({atlasUrl, experimentAccession, k, perplexity, geneId}) {
-    const atlasEndpoint = `json/experiments/${experimentAccession}/tsneplot/${perplexity}/clusters/${k}/expression/${geneId}`
-
-    return fetchResponseJson(atlasUrl, atlasEndpoint)
-      .then((responseJson) => {
-        this.setState({
-          data: responseJson,
-          errorMessage: null,
-          loadingClusters: false,
-          loadingGeneExpression: false
-        })
-      })
-      .catch((reason) => {
-        this.setState({
-          errorMessage: `${reason.name}: ${reason.message}`,
-          loadingClusters: false,
-          loadingGeneExpression: false
-        })
-      })
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.atlasUrl !== this.props.atlasUrl ||  // First two will never happen but it’s the right thing to do
-        nextProps.experimentAccession !== this.props.experimentAccession ||
-        nextProps.perplexity !== this.props.perplexity ||
-        nextProps.k !== this.props.k) {
-
-      this.setState({
-        loadingClusters: true,
-        loadingGeneExpression: true
-      })
-      this._fetchAndSetState(nextProps)
-
-    } else if (nextProps.geneId !== this.props.geneId) {
-
-      this.setState({
-        loadingGeneExpression: true
-      })
-      this._fetchAndSetState(nextProps)
-
-    }
-  }
-
-  componentDidMount() {
-    this.setState({
-      loadingClusters: true,
-      loadingGeneExpression: true
-    })
-    // Having _fetchAndSetState as callback is the right thing, but then we can’t return the promise; see tests
-    return this._fetchAndSetState(this.props)
-  }
-
-  render() {
-    const {height, atlasUrl, resourcesUrl} = this.props
-    const {suggesterEndpoint, geneId, highlightClusters, ks, k, perplexities, perplexity} = this.props
-    const {onChangePerplexity, onChangeK, onSelectGeneId} = this.props
-    const {loadingClusters, loadingGeneExpression, data, errorMessage} = this.state
-
-    return (
-      <div className={`row`}>
-        <div className={`small-12 medium-6 columns`}>
-          <ClusterTSnePlot height={height}
-                           plotData={data}
-                           perplexities={perplexities}
-                           perplexity={perplexity}
-                           onChangePerplexity={onChangePerplexity}
-                           ks={ks}
-                           k={k}
-                           onChangeK={onChangeK}
-                           highlightClusters={highlightClusters}
-                           loading={loadingClusters}
-                           resourcesUrl={resourcesUrl}
-                           errorMessage={errorMessage}
-          />
-        </div>
-
-        <div className={`small-12 medium-6 columns`}>
-          <GeneExpressionTSnePlot height={height}
-                                  plotData={data}
-                                  atlasUrl={atlasUrl}
-                                  suggesterEndpoint={suggesterEndpoint}
-                                  onSelectGeneId={onSelectGeneId}
-                                  geneId={geneId}
-                                  highlightClusters={highlightClusters}
-                                  loading={loadingGeneExpression}
-                                  resourcesUrl={resourcesUrl}
-                                  errorMessage={errorMessage}
-          />
-        </div>
-
-      </div>
-    )
-  }
-
-  componentDidCatch(error, info) {
-    this.setState({
-       errorMessage: `${error}`
-     })
-  }
-}
